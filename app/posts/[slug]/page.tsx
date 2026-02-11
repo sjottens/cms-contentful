fd5405874eab29e22f18b39e9e8690ec1c723ca1
Rogier Ottens <rvottens@gmail.com>
2026-02-11 12:26:11 +0100

    added type Props and adjust the code for it in SLUG page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -17,13 +17,15 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function PostPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+export default async function PostPage({ params }: Props) {
+  const { slug } = await params;
+
   const { isEnabled } = await draftMode();
-  const { post, morePosts } = await getPostAndMorePosts(params.slug, isEnabled);
+  const { post, morePosts } = await getPostAndMorePosts(slug, isEnabled);
 
   return (
     <div className="container mx-auto px-5">
